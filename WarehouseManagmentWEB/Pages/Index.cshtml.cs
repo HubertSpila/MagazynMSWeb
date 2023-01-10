cd8757a38b8721db7566bb80e90e485258ebdb66
Hubert <hubertspila@gmail.com>
2023-01-10 22:20:20 +0100

    Add project files.

---
+++ WarehouseManagmentWEB/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RestSharp;
+
+namespace WarehouseManagmentWEB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ WarehouseManagmentWEB/Tools/Api/Authentication.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+
+namespace WarehouseManagmentWEB.Tools.Api
+{
+    public static class Authentication
+    {
+        public static string GetToken()
+        {
+            RestClient client = new RestClient("https://localhost:7145/api/authentication");
+            //client.Timeout = -1;
+            var request = new RestRequest("https://localhost:7145/api/authentication", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            var body = @"{
+" + "\n" +
+            @"        ""username"": ""User"",
+" + "\n" +
+            @"        ""password"": ""user""
+" + "\n" +
+            @"    }";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            var response = client.Execute(request);
+            return response.Content.ToString();
+        }
+    }
+}
